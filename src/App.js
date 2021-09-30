37fba9ab4924c52c217d929554c8f76a9ee10d51
saifulislammunna <saifulmunna28@gmail.com>
2021-09-30 14:39:42 +0600

    intro-to-material-ui-added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+ 
 import './App.css';
-
+import {Button, Rating} from '@mui/material'
+import NewsPaper from './components/Newspaper/NewsPaper';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="contained">My First button</Button>
+      <Rating name="read-only" value={4} readOnly /> 
+      <NewsPaper></NewsPaper>
     </div>
   );
 }
